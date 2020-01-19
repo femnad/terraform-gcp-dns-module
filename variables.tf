13b3db8fe57b66c796a982320690a0b4685c0daa
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2020-01-19 14:11:30 +0000

    add default variables

--- variables.tf
+++ variables.tf
@@ -4,3 +4,11 @@ variable instance_ip_addr {}
 variable managed_zone {}
 variable project {}
 variable service_account_file {}
+
+variable region {
+  default = "europe-west-2"
+}
+
+variable zone {
+  default = "europe-west2-c"
+}
