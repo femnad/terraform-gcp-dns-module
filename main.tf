badd89249f7fe307ddb83c0825342e5ae59f7ddf
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2020-01-19 15:08:37 +0000

    also add record for root dns

--- main.tf
+++ main.tf
@@ -10,6 +10,16 @@ resource "google_dns_managed_zone" "zone" {
   dns_name = var.dns_root
 }
 
+resource "google_dns_record_set" "root-dns" {
+  name = var.dns_root
+  type = "A"
+  ttl  = 60
+
+  managed_zone = var.managed_zone
+
+  rrdatas = [var.instance_ip_addr]
+}
+
 resource "google_dns_record_set" "dns" {
   name = var.dns_name
   type = "A"
